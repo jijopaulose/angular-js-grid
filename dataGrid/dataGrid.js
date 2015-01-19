2ab34aa587ede94944a4470a5694dd68c721e273
sailesh.is2@gmail.com <sailesh.is2@gmail.com@fbf3ce74-7229-9670-e88a-4109f03ddef5>
2015-01-19 18:36:40 +0000

    Sorting Done

--- dataGrid/dataGrid.js
+++ dataGrid/dataGrid.js
@@ -1,9 +1,11 @@
 var gridApp=angular.module("gridApp",['ui.bootstrap']);
 
-gridApp.controller('gridController', ['$scope','$http', function ($scope,$http) {
+gridApp.controller('gridController', ['$scope','$http','$filter', function ($scope,$http,$filter) {
 
 $scope.data=null;
 $scope.header=null;
+$scope.filter=[];
+var orderBy = $filter('orderBy');
 
  $http.get("data.txt").success(function (data, status, headers, config) {
 	$scope.data=data;
@@ -17,8 +19,51 @@ $scope.header=null;
  .error(function(data, status, headers, config) {
  });
  
- $scope.setSort=function(index)
+ $scope.setSort=function(index,sort)
  {
+	$scope.header[index].sort=sort;
+	if(sort==null)
+	{
+		var i= $scope.filter.indexOf("+"+$scope.header[index].model);
+		if (i > -1) {
+			$scope.filter.splice(i, 1);
+		}
+		else
+		{
+			i= $scope.filter.indexOf("-"+$scope.header[index].model);
+			if (i > -1) {
+				$scope.filter.splice(i, 1);
+			}
+		}
+	}
+	else if(sort=='+')
+	{
+		var i= $scope.filter.indexOf("+"+$scope.header[index].model);
+		if (i == -1) {
+			var j= $scope.filter.indexOf("-"+$scope.header[index].model);
+			if (j > -1) {
+				$scope.filter.splice(j, 1,"+"+$scope.header[index].model);
+			}
+			else{
+				$scope.filter.push("+"+$scope.header[index].model);				
+			}
+		}
+	}
+	else if(sort=='-')
+	{
+		var i= $scope.filter.indexOf("-"+$scope.header[index].model);
+		if (i == -1) {
+			var j= $scope.filter.indexOf("+"+$scope.header[index].model);
+			if (j > -1) {
+				$scope.filter.splice(j, 1,"-"+$scope.header[index].model);
+			}
+			else{
+				$scope.filter.push("-"+$scope.header[index].model);				
+			}
+		}
+	}
+	$scope.data=orderBy($scope.data,$scope.filter);
+	//var filter=$scope.header[index].model;
 	
  };
 
